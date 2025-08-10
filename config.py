d044b481ede065cf47dd4de621983bc8243af3ca
Максим Василина <maksimvasilina@MacBook-Air-Maksim.local>
2025-08-10 13:34:25 +0300

    Switch to Postgres on Render

--- config.py
+++ config.py
@@ -5,12 +5,11 @@ basedir = os.path.abspath(os.path.dirname(__file__))
 class Config:
     SECRET_KEY = os.environ.get('SECRET_KEY', 'dev-key')
 
-    uri = os.environ.get('DATABASE_URL')  # беремо з Render
+    # Спочатку пробуємо власну змінну, потім стандартну від Render
+    uri = os.environ.get('RENDER_DATABASE_URL') or os.environ.get('DATABASE_URL')
+
     if uri and uri.startswith('postgresql://'):
         uri = uri.replace('postgresql://', 'postgresql+psycopg://', 1)
 
-    SQLALCHEMY_DATABASE_URI = uri or (
-        'sqlite:///instance/agro_erp.db'
-    )
-
+    SQLALCHEMY_DATABASE_URI = uri or f"sqlite:///{os.path.join(basedir, 'instance', 'agro_erp.db')}"
     SQLALCHEMY_TRACK_MODIFICATIONS = False
