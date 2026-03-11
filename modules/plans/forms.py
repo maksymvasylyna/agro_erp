b31fc5a25b90010fbb34ad19332ddfcaa25ae96c
Максим Василина <maksimvasilina@MacBook-Air-Maksim.local>
2026-03-11 15:31:24 +0200

    Редагування плану: додавання та видалення обробітків

--- modules/plans/forms.py
+++ modules/plans/forms.py
@@ -1,17 +1,19 @@
 from flask_wtf import FlaskForm
 from wtforms import SelectField, FloatField, StringField, FieldList, FormField, SubmitField
-from wtforms.validators import DataRequired
+from wtforms.validators import DataRequired, Optional
 
 class TreatmentForm(FlaskForm):
     treatment_type_id = SelectField('Вид обробітку', coerce=int, validators=[DataRequired()])
     product_id = SelectField('Продукт', coerce=int, validators=[DataRequired()])
-    rate = FloatField('Норма', validators=[DataRequired()])
-    unit = StringField('Одиниця')         # readonly в шаблоні
-    manufacturer = StringField('Виробник') # readonly в шаблоні
-    quantity = FloatField('Кількість')     # розраховується
+    rate = FloatField('Норма', validators=[Optional()])
+
+    unit = StringField('Одиниця', validators=[Optional()])
+    manufacturer = StringField('Виробник', validators=[Optional()])
+    quantity = FloatField('Кількість', validators=[Optional()])
 
     class Meta:
-        csrf = False  # ✅ Вимикаємо CSRF для підформи
+        csrf = False
+
 
 class PlanForm(FlaskForm):
     treatments = FieldList(FormField(TreatmentForm), min_entries=0)
